13530779b46485e2ca42662f8bb750540968228f
AbrarKhan1993 <er.khanabrar@gmail.com>
2022-10-01 13:35:27 +0530

    changes in model

--- src/main/java/com/practice/mysqlcrud/model/Student.java
+++ src/main/java/com/practice/mysqlcrud/model/Student.java
@@ -5,12 +5,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@Builder
+@NoArgsConstructor
 public class Student {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
