62fa725bfd9b34642943ba163fa81dd530d79439
AbrarKhan1993 <er.khanabrar@gmail.com>
2022-10-02 13:05:34 +0530

    changes in test class

--- src/test/java/com/practice/mysqlcrud/MysqlcrudApplicationTests.java
+++ src/test/java/com/practice/mysqlcrud/MysqlcrudApplicationTests.java
@@ -1,13 +1,13 @@
-package com.practice.mysqlcrud;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest()
-class MysqlcrudApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.practice.mysqlcrud;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest()
+//class MysqlcrudApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
